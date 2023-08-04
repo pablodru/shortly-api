132e421f0981086622485eba45d2f9c7b80039c7
pablodru <pdrocha03@gmail.com>
2023-08-04 13:21:25 -0300

    feat: ranking of urls ordened by visitCount

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -22,6 +22,16 @@ export async function getUrlsByUser (req, res) {
 export async function ranking (req, res) {
     try {
 
+        const result = await db.query(
+            `SELECT users.id, users.name, COUNT(urls."userId") AS "linksCount", SUM(urls."visitCount") AS "visitCount" FROM users
+                LEFT JOIN urls ON users.id=urls."userId"
+                GROUP BY users.id
+                ORDER BY "visitCount" DESC
+                LIMIT 10`
+        )
+
+        res.send(result.rows)
+
     } catch (err) {
         res.status(500).send(err.message);
     }
